58b739d5e5941eb9d2c09f73613e14a88d611c58
JopsonDev <jopsonmason@outlook.com>
2026-04-22 13:26:47 -0400

    added sorter with teacher help

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -3,14 +3,20 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class SearchInventory {
     public static void main(String[] args) {
         String fileName = "inventory.csv";
         try {
+
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
 
             String line;
+
+            List<Product> inventory = new ArrayList<>();
 
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split("\\|");
@@ -18,10 +24,15 @@ public class SearchInventory {
                 int id = Integer.parseInt(parts[0]);
                 String name = parts[1];
                 double price = Double.parseDouble(parts[2]);
-                ArrayList<Product> inventory = getInventory(id, name, price);
-                for (Product product : inventory) {
-                    System.out.println(product);
+                inventory.add(new Product(id, name, price));
+
                 }
+            for (Product product : inventory) {
+                System.out.println(product);
+            }
+            Collections.sort(inventory, Comparator.comparing(Product::getName));
+            for (Product product : inventory) {
+                System.out.println(product);
             }
         } catch (Exception a) {
             System.out.println("explosions everywhere!!!!");
@@ -29,9 +40,8 @@ public class SearchInventory {
 
     }
 
-    public static ArrayList<Product> getInventory(int id, String name, double price) {
+    public static List<Product> getInventory() {
         ArrayList<Product> inventory = new ArrayList<>();
-        inventory.add(new Product(id, name, price));
 
         return inventory;
     }

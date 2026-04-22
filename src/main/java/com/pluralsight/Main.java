898f8f1882e0ebecde27ae67137e7ccbca110145
JopsonDev <jopsonmason@outlook.com>
2026-04-22 11:29:08 -0400

    almost lost it push

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,14 +2,21 @@ package com.pluralsight;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.List;
 
 public class Main {
     static void main(String[] args) {
         String fileName = "inventory.csv";
-        try {
+        List<Product> inventory = SearchInventory.getInventory();
+
+        for (Product product : inventory) {
+            System.out.println(product);
+        }
+/*        try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
         } catch (Exception a) {
             System.out.println("explosions everywhere!!!!");
-        }
+        }*/
     }
 }
--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -14,8 +14,12 @@ public class SearchInventory {
 
     public static ArrayList<Product> getInventory() {
         ArrayList<Product> inventory = new ArrayList<>();
+        inventory.add(new Product(1234, "apple", 22.99));
+        inventory.add(new Product(2345, "time", 27.99));
+        inventory.add(new Product(3456, "ugh", 299.99));
+        inventory.add(new Product(4567, "wait here", 21.99));
+        inventory.add(new Product(5678, "top", 13.99));
 
-        // Write your code here
 
         return inventory;
     }
